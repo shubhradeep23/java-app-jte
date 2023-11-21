3164823d8b891c37e310ed249ee6178515096cc3
shubhradeep23 <devops.april.2022@gmail.com>
2023-11-21 11:02:29 +0530

    java app repo for jte

---
+++ src/main/java/com/example/k8s/springbootkubernetes/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.example.k8s.springbootkubernetes.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@RequestMapping("/greeting")
+	public String greeting() {
+		return "Deploy Java App to AWS EKS by Shubhradeep Ghosh";
+	}
+
+}
